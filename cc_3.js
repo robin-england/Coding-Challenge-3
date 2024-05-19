d62efeb135ffc3fd3b9a7e3c4857bf1f8ff25496
robin-england <robin.england94@googlemail.com>
2024-05-19 17:59:08 -0400

    Test accelerate and brake methods on Car instances.

--- cc_3.js
+++ cc_3.js
@@ -1,5 +1,4 @@
 // 1. Car Class Constructor:
-
 class Car{
     constructor(make,speed){
         this.make=make
@@ -8,13 +7,28 @@ class Car{
     // 2. Accelerate Method:
     accelerate(){
         this.speed = (this.speed + 10)  // Adding 10 km/h
-        console.log("You are now driving at " + this.speed + "km/h."
-        );
+        console.log("We are accelerating! We are now driving at " + this.speed + "km/h.")
     }
     // 3. Brake Method:
     brake(){
         this.speed = (this.speed - 5)  // Subtracting 5 km/h
-        console.log("You are now driving at " + this.speed + "km/h."
-        );
+        console.log("We are slowing down! We are now driving at " + this.speed + "km/h.")
     }
 }
+// 4. Create and Test Car Objects:
+let car1 = new Car ("BMW", 120)
+let car2 = new Car ("Mercedes", 95)
+
+car1.accelerate()   // Testing accelerate and brake methods for BMW
+car1.brake()
+car1.accelerate()
+car1.brake()
+car1.brake()
+console.log(car1)   // Displaying final speed for BMW. Final speed should be 125km/h.
+
+car2.accelerate()   // Performing tests for Mercedes
+car2.accelerate()
+car2.brake()
+car2.accelerate()
+car2.brake()
+console.log(car2)   // Displaying final speed for Mercedes. Final speed should be 115km/h.
