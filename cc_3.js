47d4536eeeeddc92fa884123d9f5199701c844de
robin-england <robin.england94@googlemail.com>
2024-05-19 17:44:03 -0400

    Add brake method to decrease speed by 5 km/h.

--- cc_3.js
+++ cc_3.js
@@ -4,9 +4,16 @@ class Car{
     constructor(make,speed){
         this.make=make
         this.speed=speed
-    }
+    }       
+    // 2. Accelerate Method:
     accelerate(){
-        this.speed = (this.speed + 10)
+        this.speed = (this.speed + 10)  // Adding 10 km/h
+        console.log("You are now driving at " + this.speed + "km/h."
+        );
+    }
+    // 3. Brake Method:
+    brake(){
+        this.speed = (this.speed - 5)  // Subtracting 5 km/h
         console.log("You are now driving at " + this.speed + "km/h."
         );
     }
